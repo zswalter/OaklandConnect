8911da7ba9928d0b05945abda84931c3f783cd6b
Zachary Walters <zachary0191@gmail.com>
2019-04-09 20:12:52 -0400

    add different views based on user type

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,36 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 # Create your models here.
 class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        (1, 'student'),
+        (2, 'tutor'),
+        (3, 'professor'),
+    )
+
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
 
     def __str__(self):
         return self.username
+    def is_student(self):
+        user = self
+        if user.user_type == 1:
+            return True
+        else:
+            return False
+
+    def is_tutor(self):
+        user = self
+        if user.user_type == 2:
+            return True
+        else:
+            return False
+
+    def is_professor(self):
+        user = self
+        if user.user_type == 3:
+            return True
+        else:
+            return False
+
