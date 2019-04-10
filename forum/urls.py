f39a257ff68a754bde222180ec85dd586f1a5417
Zachary Walters <zachary0191@gmail.com>
2019-04-10 02:04:39 -0400

    fix deleting comments

--- forum/urls.py
+++ forum/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('forum/<int:pk>/comment/', views.post_comment, name='post_comment'),
     path('', views.forum_list, name='forum_list'),
     path('forum/<int:pk>/delete/', views.post_delete, name='post_delete'),
-    path('forum/<int:pk>/delete/', views.comment_delete, name='comment_delete'),
+    path('comment/<int:pk>/delete/', views.comment_delete, name='comment_delete'),
 ]
